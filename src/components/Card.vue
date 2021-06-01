29cc6ac1d90e73d93b990bd2972a4f7578a068a9
Ricardo Dias <slo.motion@gmail.com>
2021-06-01 18:09:50 +0100

    First commit!

---
+++ src/components/Card.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-1/2 p-4 border border-gray-200 rounded-md shadow-md">
+        <todo-imput />
+    </div>
+</template>
+
+<script>
+import TodoImput from "./TodoImput";
+export default {
+    components: {
+        TodoImput,
+    },
+};
+</script>
---
+++ src/components/TodoImput.vue
@@ -0,0 +1,7 @@
+<template>
+    <input class="w-full" type="text" placeholder="Insert a new todo" />
+</template>
+
+<script>
+export default {};
+</script>
