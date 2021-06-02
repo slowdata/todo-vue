847390d6404d0c7354c4fff10b9faa9196c5b1c7
Ricardo Dias <slo.motion@gmail.com>
2021-06-02 14:47:53 +0100

    Add submit button

--- src/components/TodoImput.vue
+++ src/components/TodoImput.vue
@@ -1,11 +1,20 @@
 <template>
-    <input
-        class="w-full px-2 py-3 mb-6 rounded-md 
-        focus:outline-none focus:ring-2
-        focus:ring-gray-300"
-        type="text"
-        placeholder="Insert a new todo"
-    />
+    <form action="" class="flex">
+        <input
+            class="w-full px-2 py-3 rounded-md
+            focus:outline-none focus:ring-2
+            focus:ring-gray-300"
+            type="text"
+            placeholder="Insert a new todo"
+            v-bind="todo"
+        />
+        <button
+            class="ml-2 px-4 border border-gray-300 rounded-lg
+        hover:bg-green-600 hover:text-white"
+        >
+            Add
+        </button>
+    </form>
 </template>
 
 <script>
